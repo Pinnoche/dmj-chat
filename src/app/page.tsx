82a0d113088f8d16c3e009ad7dc4a3c85ee729c2
TherealJoeman1 <undisputedsuperjoe@gmail.com>
2025-11-24 10:17:18 +0100

    mobile reponsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [isDMJChat, setIsDMJChat] = useState<boolean>(true);
   const chatTitle = isDMJChat ? "DMJ CHAT" : "ZUBIN CHAT";
   return (
-    <div className="w-full h-screen flex bg-black font-sans dark:bg-black text-white">
+    <div className=" w-full h-screen flex bg-black font-sans dark:bg-black text-white">
       <SideBar />
       <main className="w-[80%] bg-[#212121] ml-[20%] flex flex-col">
         <NavBar chatTitle={chatTitle} setIsDMJChat={setIsDMJChat} />
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,10 +7,10 @@ export default function SideBar() {
     window.open("https://x.com/dmj_wise", "_blank");
   };
   return (
-    <div className="w-[20%] border-r border-white/60 p-4 fixed top-0 left-0 h-full flex flex-col gap-8">
+    <div className="w-[20%]  border-r border-white/60 p-4 fixed top-0 left-0 h-full flex flex-col gap-8">
       <div
         onClick={handleX}
-        className="rounded-full w-16 h-16 border border-white/30 cursor-pointer"
+        className="rounded-full w-16 h-16 border border-white/30 cursor-pointer max-sm:w-12 max-sm:h-12 overflow-hidden"
       >
         <Image
           src="https://pbs.twimg.com/profile_images/1651878382072733697/KtLYM224_400x400.jpg"
@@ -18,11 +18,11 @@ export default function SideBar() {
           priority
           width={64}
           height={64}
-          className="w-full h-full rounded-full"
+          className="w-full h-full rounded-full "
         />
       </div>
       <div className="w-full h-full flex items-center justify-center">
-        <p className="rotate-90 font-semibold animate-pulse text-2xl text-center whitespace-nowrap">
+        <p className="max-sm:text-[20px] rotate-90 font-semibold animate-pulse text-2xl text-center whitespace-nowrap">
           CHAT MEMORY COMING SOON!
         </p>
       </div>
@@ -31,7 +31,7 @@ export default function SideBar() {
         <a
           href="https://x.com/sentientagi"
           target="_blank"
-          className="font-semibold text-white"
+          className="font-semibold text-white max-sm:text-[8px]"
         >
           SentientAGI ROMA v2
         </a>{" "}
