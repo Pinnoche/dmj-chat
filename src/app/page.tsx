d5e99a9c4f01e820d8c7a2c30503fc9b7e518e9f
pinnoche <ptime552@gmail.com>
2026-01-09 15:14:07 +0100

    ft: page loader added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,44 @@ import Modal from "@/components/Modal";
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
 import { apiClient } from "@/lib/api-client";
+
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isDMJChat, setIsDMJChat] = useState<boolean>(true);
-
-  const fetchUser = async () => {
-    try {
-      await apiClient.get("/auth/me");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [errorStat, setErrorStat] = useState<string | number>(true);
   const chatTitle = isDMJChat ? "DMJ CHAT" : "ZUBIN CHAT";
   const [openDonateModal, setOpenDonateModal] = useState<boolean>(false);
   const [isBlur, setIsBlur] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setIsLoading(true);
+        await apiClient.get("/auth/me");
+        setIsLoading(false);
+      } catch (error: any) {
+        setErrorStat(error.response?.status || "Unknown error");
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (isLoading || errorStat === 401) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center bg-primary text-text-primary">
+        <div className="flex flex-col items-center">
+          <div className="rounded-full border-8 border-t-transparent border-gray-200 h-32 w-32 mb-4 animate-spin"></div>
+          <h2 className="text-xl font-semibold">Loading...</h2>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full h-screen flex bg-primary font-sans text-text-primary">
       {isBlur && (
