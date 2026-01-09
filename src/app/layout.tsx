7d685fb024712a7a468261ac97714fd14a3e8448
pinnoche <ptime552@gmail.com>
2026-01-09 04:49:35 +0100

    metadata added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,52 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DMJ CHAT",
+  metadataBase: new URL("https://tradeoninsideroption.com"),
+
+  title: {
+    default: "DMJ's DegenXpert | AI Chat Interface",
+    template: "%s | DMJ's DegenXpert",
+  },
+
   description:
     "A chat interface developed by DMJ for seamless interactions between users and several AI Agent models.",
+
+  alternates: {
+    canonical: "https://degenxpert.xyz",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  openGraph: {
+    title: "DMJ's DegenXpert | AI Chat Interface",
+    description:
+      "A chat interface developed by DMJ for seamless interactions between users and several AI Agent models.",
+    url: "https://degenxpert.xyz",
+    images: [
+      {
+        url: "https://degenxpert.xyz/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "DegenXpert - AI Chat Interface",
+      },
+    ],
+    siteName: "DegenXpert",
+    type: "website",
+  },
+
+  // icons: {
+  //   icon: "/icon.png",
+  //   shortcut: "/favicon.ico",
+  //   apple: "/apple-icon.png",
+  // },
+
+  // twitter: {
+  //   card: "summary_large_image",
+  //   site: "@insideroption25",
+  // },
 };
 
 export default function RootLayout({
